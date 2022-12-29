f104afe01e31ab5c754c79a02ac045ae2353c107
KatiBar <katarzyna.kinga.baranska@gmail.com>
2022-12-29 07:23:09 +0100

    Dodanie logiki w klasie Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -8,6 +8,89 @@ public class Main {
     }
 
     public void run(Scanner scanner) {
-        // napisz swój program tutaj. Do wczytywania danych użyj przekazanego w parametrze scannera
+        try {
+            List<Integer> numbersArray = createNumbersArray(scanner);
+            List<Integer> invertedList = invertList(numbersArray);
+            printList(invertedList);
+            int sum = calculateSum(numbersArray);
+            printSumResults(numbersArray, sum);
+            int biggestNumber = calculateBiggestNumber(numbersArray);
+            int smallestNumber = calculateSmallestNumber(numbersArray);
+            System.out.println("Najmniejsza liczba w liście to " + smallestNumber);
+            System.out.println("Największa liczba w liście to " + biggestNumber);
+        } catch (InputMismatchException e) {
+            System.err.println("Nie podałeś liczby całkowitej");
+        } finally {
+            scanner.close();
+        }
+    }
+
+    private static List<Integer> createNumbersArray(Scanner scanner) throws InputMismatchException {
+        List<Integer> numbersList = new ArrayList<Integer>();
+        int number = -1;
+        do {
+            System.out.println("Podaj dodatnią liczbę całkowitą: ");
+            number = scanner.nextInt();
+            if (number >= 0) {
+                numbersList.add(number);
+            }
+        } while (number >= 0);
+        return numbersList;
+    }
+
+    private static List<Integer> invertList(List<Integer> list) {
+        List<Integer> invertedList = new ArrayList<>(list.size());
+        for (int i = 0; i < list.size(); i++) {
+            invertedList.add(list.get(list.size() - 1 - i));
+        }
+        return invertedList;
+    }
+
+    private static void printList(List<Integer> list) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < list.size() - 1; i++) {
+            stringBuilder.append(list.get(i));
+            stringBuilder.append(", ");
+        }
+        stringBuilder.append(list.get(list.size() - 1));
+        System.out.println(stringBuilder);
+    }
+
+    private static int calculateSum(List<Integer> list) {
+        int sum = 0;
+        for (Integer number : list) {
+            sum += number;
+        }
+        return sum;
+    }
+
+    private static void printSumResults(List<Integer> list, int sum) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < list.size() - 1; i++) {
+            stringBuilder.append(list.get(i));
+            stringBuilder.append(" + ");
+        }
+        stringBuilder.append(list.get(list.size() - 1) + " = " + sum);
+        System.out.println(stringBuilder);
+    }
+
+    private static int calculateBiggestNumber(List<Integer> list) {
+        int biggestNumber = 0;
+        for (Integer number : list) {
+            if (number > biggestNumber) {
+                biggestNumber = number;
+            }
+        }
+        return biggestNumber;
+    }
+
+    private static int calculateSmallestNumber(List<Integer> list) {
+        int smallestNumber = list.get(0);
+        for (Integer number : list) {
+            if (number < smallestNumber) {
+                smallestNumber = number;
+            }
+        }
+        return smallestNumber;
     }
 }
